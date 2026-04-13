0a6ab9028f16e83793573cb577755d8b5b2c1b75
thirdu9 <g.t.thiryou@gmail.com>
2026-04-14 08:27:50 +0930

    update startup service registrator

--- PluginServiceRegistrator.cs
+++ PluginServiceRegistrator.cs
@@ -2,6 +2,7 @@ using MediaBrowser.Controller;
 using MediaBrowser.Controller.Plugins;
 using Microsoft.Extensions.DependencyInjection;
 using SymSmartQueue.Data;
+using SymSmartQueue.Startup;
 
 namespace SymSmartQueue
 {
@@ -11,7 +12,8 @@ namespace SymSmartQueue
         // If your agent gets a compilation error regarding the implementation, simply remove the second parameter.
         public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
         {
-            // Register our database engine as a Singleton so all API calls and background tasks share the same SQLite connection logic.
+            // Register database engine as a Singleton so all API calls and background tasks share the same SQLite connection logic.
+            serviceCollection.AddHostedService<SymBootstrapper>();
             serviceCollection.AddSingleton<DatabaseManager>();
         }
     }
